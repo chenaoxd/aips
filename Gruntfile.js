8f7b79b7266ece8a985cc554fda4a684e624b279
dreamszl <chenao3220@gmail.com>
2014-01-06 21:01:39 +0800

    A bug version

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,11 +13,27 @@ module.exports = function(grunt) {
                 }
             }
         },
-        watch: { /* nothing to do in watch anymore */ }
+        less: {
+            development: {
+                options: {
+                    paths: ['./public/less'],
+                    yuicompress: true
+                },
+                files: {
+                    './public/css/test.css': './public/less/test.less'
+                }
+            }
+        },
+        watch: {
+            files: "./public/less/*.less",
+            tasks: ['less']
+        }
     });
 
+
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-nodemon');
 
-    grunt.registerTask('default', ['nodemon']);
+    grunt.registerTask('default', ['less','watch']);
 };
